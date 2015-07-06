c2b85a9abaf9c1a652bf1348469800d8b0eb8dc3
simioliolio <simioliolio@bitbasic.co.uk>
2015-07-06 10:36:21 +0100

    Initial Commit

---
+++ ContentCarousel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ContentCarousel
+//
+//  Created by Simon Haycock on 06/07/2015.
+//  Copyright (c) 2015 oxygn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
